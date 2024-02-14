53205c4a6d50659acc576510996254c93136e668
穗乃果 <1763575579@qq.com>
2024-02-14 19:23:00 +0800

    update

--- Server/Program.cs
+++ Server/Program.cs
@@ -10,10 +10,10 @@ public class Program
     {
         using var md5 = MD5.Create();
 
-        using var fbmodFs = File.OpenRead(".\\Mods\\AllKitAndHero-2024NewYear-32v32-v3.0.0.0.fbmod");
+        using var fbmodFs = File.OpenRead(".\\Mods\\AllKitAndHero-2024NewYear-32v32-v3.2.0.0.fbmod");
         var fbmodMd5 = md5.ComputeHash(fbmodFs);
 
-        Console.WriteLine("AllKitAndHero-2024NewYear-32v32-v3.0.0.0.fbmod");
+        Console.WriteLine("AllKitAndHero-2024NewYear-32v32-v3.2.0.0.fbmod");
         Console.WriteLine(BitConverter.ToString(fbmodMd5).Replace("-", ""));
         Console.WriteLine();
 
