aa0d36003d2d401503bcd8829cac2620faaf81a2
穗乃果 <1763575579@qq.com>
2024-03-11 20:56:29 +0800

    update

---
+++ MarneMapList/MapData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MarneMapList;
+
+public static class MapData
+{
+    public static Dictionary<string, string[]> MapAndModeInfo = [];
+
+    static MapData()
+    {
+        MapAndModeInfo.Add("MP_Amiens", ["Conquest0", "Rush0", "BreakThrough0", "BreakThroughLarge0", "Possession0", "TugOfWar0", "Domination0", "TeamDeathMatch0"]);
+        MapAndModeInfo.Add("MP_Chateau", ["Conquest0", "Rush0", "BreakThrough0", "BreakThroughLarge0", "Possession0", "TugOfWar0", "Domination0", "TeamDeathMatch0"]);
+        MapAndModeInfo.Add("MP_Desert", ["Conquest0", "Rush0", "BreakThrough0", "BreakThroughLarge0", "Possession0", "TugOfWar0", "Domination0", "TeamDeathMatch0"]);
+    }
+}
---
+++ MarneMapList/Models/MapInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MarneMapList.Models;
+
+public class MapInfo
+{
+    public string DLC { get; set; }
+    public string Name { get; set; }
+    public string ChsName { get; set; }
+    public string Url { get; set; }
+    public List<string> Mode { get; set; }
+
+    public MapInfo()
+    {
+        Mode = [];
+    }
+}
